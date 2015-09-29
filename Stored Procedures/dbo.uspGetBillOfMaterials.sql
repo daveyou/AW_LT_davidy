2358b76096ee80992c9d94d64d5b73cbee236d1a
David You <david.you@red-gate.com>
2015-09-29 10:53:57 -0700

    modified sproc

--- Stored Procedures/dbo.uspGetBillOfMaterials.sql
+++ Stored Procedures/dbo.uspGetBillOfMaterials.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -37,9 +38,10 @@ BEGIN
     FROM [BOM_cte] b
     GROUP BY b.[ComponentID], b.[ComponentDesc], b.[ProductAssemblyID], b.[BOMLevel], b.[RecursionLevel], b.[StandardCost], b.[ListPrice]
     ORDER BY b.[BOMLevel], b.[ProductAssemblyID], b.[ComponentID]
-    OPTION (MAXRECURSION 25) 
+    OPTION (MAXRECURSION 77) 
 END;
 GO
+
 EXEC sp_addextendedproperty N'MS_Description', N'Stored procedure using a recursive query to return a multi-level bill of material for the specified ProductID.', 'SCHEMA', N'dbo', 'PROCEDURE', N'uspGetBillOfMaterials', NULL, NULL
 GO
 EXEC sp_addextendedproperty N'MS_Description', N'Input parameter for the stored procedure uspGetBillOfMaterials used to eliminate components not used after that date. Enter a valid date.', 'SCHEMA', N'dbo', 'PROCEDURE', N'uspGetBillOfMaterials', 'PARAMETER', N'@CheckDate'
