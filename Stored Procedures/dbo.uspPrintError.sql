e01363fdb379acc22a62293c3ab3266a7325b121
unknown <david.you@RED-GATE>
2016-03-10 14:13:18 -0800

    modified sproc

--- Stored Procedures/dbo.uspPrintError.sql
+++ Stored Procedures/dbo.uspPrintError.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -13,7 +14,7 @@ BEGIN
     SET NOCOUNT ON;
 
     -- Print error information. 
-    PRINT 'Error ' + CONVERT(varchar(50), ERROR_NUMBER()) +
+    PRINT 'Error ' + CONVERT(varchar(90), ERROR_NUMBER()) +
           ', Severity ' + CONVERT(varchar(5), ERROR_SEVERITY()) +
           ', State ' + CONVERT(varchar(5), ERROR_STATE()) + 
           ', Procedure ' + ISNULL(ERROR_PROCEDURE(), '-') + 
@@ -21,5 +22,6 @@ BEGIN
     PRINT ERROR_MESSAGE();
 END;
 GO
+
 EXEC sp_addextendedproperty N'MS_Description', N'Prints error information about the error that caused execution to jump to the CATCH block of a TRY...CATCH construct. Should be executed from within the scope of a CATCH block otherwise it will return without printing any error information.', 'SCHEMA', N'dbo', 'PROCEDURE', N'uspPrintError', NULL, NULL
 GO
